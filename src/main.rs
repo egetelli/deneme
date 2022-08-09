58751f4593c5cda5fed572b312f3c3922073aede
egetelli <egetelli@hotmail.com>
2022-08-09 17:31:16 +0300

    çalışan ve bitmiş hali(sonunda)

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,36 @@ struct Arabalar{
 
 fn main() {
     
-    
+    let mut ag = Arabalar {Honda: 10000, Mercedes: 30000, Bmw: 25000, Toyota: 15000};
+    println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda,ag.Mercedes,ag.Bmw,ag.Toyota);
 
+    for i in 1..100{
 
-    loop{
-
-    println!("Sizce zam mı gelmeli indirim mi ? (z/i)");
+    println!("Sizce zam mı gelmeli indirim mi ? (indirim için 2, zam için 1)");
 
     let mut answer = String::new();
     io::stdin().read_line(&mut answer);
     let answer = answer.trim().parse::<i32>().expect("invalid input");
-    greet(&answer);
-     
-}
+    
+    if answer == 1{
+        println!("YENI ARABA FIYATLARI");
+        ag.Honda += 5000;
+        ag.Mercedes += 5000;
+        ag.Bmw += 5000;
+        ag.Toyota += 5000;
+        println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda,ag.Mercedes,ag.Bmw,ag.Toyota);
+    }
+    else if answer == 2{
+        println!("YENI ARABA FIYATLARI");
+        ag.Honda -= 5000;
+        ag.Mercedes -= 5000;
+        ag.Bmw -= 5000;
+        ag.Toyota -= 5000;
+        println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda,ag.Mercedes,ag.Bmw,ag.Toyota);
+    }
+
+    }
+
 
 }
 
@@ -40,4 +57,20 @@ fn greet(answer: &i32) {
         }
         _ => println!("Invalid input: {}", answer),
     }
+}
+
+
+fn greet1(answer: &i32){
+    let mut ag = Arabalar {Honda: 10000, Mercedes: 30000, Bmw: 25000, Toyota: 15000};
+    println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda,ag.Mercedes,ag.Bmw,ag.Toyota);
+
+    if *answer == 1{
+        println!("YENI ARABA FIYATLARI");
+        println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda+5000,ag.Mercedes+5000,ag.Bmw+5000,ag.Toyota+5000);
+    }
+    else if *answer == 2{
+        println!("YENI ARABA FIYATLARI");
+        println!("Honda'nın fiyatı {} $, Mercedes'nın fiyatı {} $, Bmw'nın fiyatı {} $, Toyota'nın fiyatı {} $",ag.Honda-5000,ag.Mercedes-5000,ag.Bmw-5000,ag.Toyota-5000);
+    }
+
 }
